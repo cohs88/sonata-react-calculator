56a183b2cf5194359cd13561bf8fcf3922ebb82c
servando coria <servando.coria@sonatasmx.com>
2019-06-04 11:44:47 -0700

    key props

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Key = () => {
+const Key = (props) => {
     return (
-        <div>Key</div>
+        <div>{props.text}</div>
     );
 };
 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,14 +5,14 @@ const Keypad  = (props) =>{
     return (
         <div>
             Keypad
-            <Key/>
-            <Key/>
-            <Key/>
-            <Key/>
-            <Key/>
-            <Key/>
-            <Key/>
-            <Key/>
+            <Key text="1"/>
+            <Key text="2"/>
+            <Key text="3"/>
+            <Key text="+"/>
+            <Key text="4"/>
+            <Key text="5"/>
+            <Key text="6"/>
+            <Key text="-"/>
         </div>
     );
 };
