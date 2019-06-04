f884157697a433d2b3e76f0a0c4bd819062898f8
servando coria <servando.coria@sonatasmx.com>
2019-06-04 10:50:06 -0700

    added simple keypad component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Result from './Result';
+import Keypad from './Keypad';
 
 class App extends React.Component{
     render(){
         return (
             <div>
                 <Result />
+                <Keypad />
             </div>
         );
     }
---
+++ src/components/Keypad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Keypad  = (props) =>{
+    return (
+        <div>
+            Keypad
+        </div>
+    );
+};
+
+export default Keypad;
