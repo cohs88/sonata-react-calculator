724bf05f003df36e5234c409e4355a8b7ae029a3
servando coria <servando.coria@sonatasmx.com>
2019-06-04 11:16:46 -0700

    changed Result component to class based; Added timer and state message

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const Result = (props) => {
-    return (
-        <div>
-            result
-        </div>
-    );
+class Result extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = { message: 'HELLO' };
+    }
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({message: 'servando'});
+        }, 30000); // time in milliseconds
+    }
+    render(){
+        return (
+            <div>
+                {this.state.message}
+            </div>
+        );
+    }
 };
 
 export default Result;
